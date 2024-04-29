cce61bc42458ba9ac3bdafb245483f9c58bb8d10
zjun <565061763@qq.com>
2024-04-29 11:14:43 +0800

    1.0.0-alpha04

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-scratchcard"
-val libVersion = "1.0.0-alpha03"
+val libVersion = "1.0.0-alpha04"
 
 android {
     namespace = "com.sd.lib.compose.scratchcard"
